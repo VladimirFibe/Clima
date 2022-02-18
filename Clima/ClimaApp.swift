c215b24d9f57688b3a4a37a518fd420af8d08d7c
Vladimir Fibe <motiw@icloud.com>
2022-02-18 18:45:45 +0600

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by Vladimir Fibe on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
